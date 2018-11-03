5ce05ceddb9680226791974b05c7f49f8ddedd8c
Alyssa Cipriano <alyssacipriano@gmail.com>
2018-11-02 23:22:57 -0700

    Fill in code to make test pass

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,33 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var matchingClass = [];
+
+  var bod = document.body;
+
+  //will call the recursive function if first element has a child node
+  if (bod.classList && bod.classList.contains(className)){
+  	matchingClass.push(bod);
+  	if (bod.childNodes[0]){
+    	moreNodes(bod);
+    }
+  } else if (bod.childNodes[0]){
+  	moreNodes(bod);
+  }
+  
+  //recursive function; will call itself if element has a child node
+  function moreNodes(element){
+    var currNode = element.childNodes
+  	for (var i = 0; i < element.childNodes.length; i++){
+  		if (currNode[i].classList && currNode[i].classList.contains(className)){
+        matchingClass.push(currNode[i]);
+      } if (currNode[i].childNodes[0]){
+        moreNodes(currNode[i]);
+      }
+  	}
+  }
+  
+  return matchingClass;
+
 };
